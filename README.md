79f466fcb67f23941d4afd88a87e4697bf8c3387
Navraj Chohan <nlake44@gmail.com>
2013-04-22 09:42:24 -0700

    Updated thread to run tools

--- README.md
+++ README.md
@@ -11,13 +11,27 @@ are not familiar with general cloud principles or those who are not
 comfortable working with a traditional command line interface can get
 started with deploying AppScale clouds and AppScale cloud applications.
 
-AppsCake has been developed using the Ruby programming language and is
-based on Sinatra. So far it has been tested in Xen and EC2 cloud
-environments.
+AppsCake has been developed using the python programming language and is
+based on Django. 
 
 ## Prerequisites ##
 - Python 2.7
 - Django 1.5 (get the tar ball here: https://www.djangoproject.com/download/1.5.1/tarball/)
+
+# Install process ##
+Run ```bash get\_tools.sh```.
+
+### For Mac OSX ###
+Install the tools by going into appscale-tools/osx
+```bash appscale_install.sh```
+
+### For Debian based systems ###
+Install the tools by going into appscale-tools/debian and running
+```bash appscale_install.sh```
+
+### Other distros ###
+See the install script for MacOSX for the required dependencies. Contact us if you 
+have problems at: support@appscale.com or visit our IRC channel #appscale on freenode.net.
 
 License
 -------
--- appscake/views.py
+++ appscake/views.py
@@ -1,13 +1,16 @@
+""" Views for different pages. """
 import logging
 
 from appscake import helpers
-from django.contrib import  messages
-from django.shortcuts import render
-from django.shortcuts import render_to_response
+from appscake import create_instances
 from appscake.forms import CommonFields
 from appscake.forms import Cluster
+
+from django.contrib import  messages
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 
 # When deploying on virtual machines without IaaS support.
@@ -15,6 +18,9 @@ CLUSTER_DEPLOY = "cluster"
 
 # When deploying on IaaS.
 CLOUD_DEPLOY = "cloud"
+
+# A global variable to store all threads running the tools.
+ALL_THREADS = {}
 
 def home(request):
   return render(request, 'base/home.html', {'form': CommonFields(),})
@@ -25,11 +31,17 @@ def about(request):
 def virtualbox_form(request):
   return render(request, 'base/virtualbox.html')
 
+def get_status(request):
+  return create_instances.get_status()
+
 def start(request):
   """ This is the page a user submits a request to start AppScale. """
   if request.method == 'POST':
-
     form = CommonFields(data=request.POST)
+    if not form.is_valid():
+      return HttpResponseRedirect('/start/?error=badform') # Redirect after POST
+
+    tools_runner = None
 
     email = form['admin_email']
     password = form['admin_pass']
@@ -39,21 +51,37 @@ def start(request):
     if cloud_type == CLOUD_DEPLOY:
       infras = form['infrastructure'] 
       deployment_type = form['deployment_type']
+      machine = form['machine']
       if deployment_type == ADVANCE_DEPLOYMENT:
         ips_yaml = form['ips_yaml']
+        tools_runner = ToolsRunner(cloud_type,
+                                   keyname,
+                                   email,
+                                   password,
+                                   placement=ADVANCE_DEPLOYMENT,
+                                   machine=machine,
+                                   instance_type=instance_type,
+                                   ips_yaml=ips_yaml)
       elif deployment_type == SIMPLE_DEPLOYMENT:
         min_nodes = form['min']
         max_nodes = form['max']
-      machine = form['machine']
+        tools_runner = ToolsRunner(cloud_type,
+                                   keyname,
+                                   email,
+                                   password,
+                                   placement=SIMPLE_DEPLOYMENT,
+                                   machine=machine,
+                                   instance_type=instance_type,
+                                   min_nodes=min_nodes,
+                                   max_nodes=max_nodes)
     elif cloud_type == CLUSTER_DEPLOY:
       ips_yaml = form['ips_yaml']
-
-    if not form.is_valid():
-      return HttpResponseRedirect('/start/?error=badform') # Redirect after POST
-      # Fire it off in a thread and then redirect to a page that polls.
-
+      tools_runner = ToolsRunner(cloud_type,
+                                 keyname,
+                                 email,
+                                 password,
+                                 ips_yaml=ips_yaml)
+    tools_runner.start()
+    ALL_THREADS[tools_runner.identifier] = tools_runner
   else:
     return  render(request, 'base/start.html')
-
-
-
